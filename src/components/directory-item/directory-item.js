4c070165dd0dc944b58a0a692eda7d7c8ec42629
Ivan Havryliuk <ihavryliuk@gomage.com>
2023-06-03 16:15:51 +0300

    update homepage navigation

--- src/components/directory-item/directory-item.js
+++ src/components/directory-item/directory-item.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import {useNavigate} from 'react-router-dom'
 import {DirectoryItemContainer, Body, BackgroundImage} from './directory-item.styles'
 // import './directory-item.scss'
 
-const DirectoryItem = ({title, imageUrl}) => {
+const DirectoryItem = ({category}) => {
+    const {route, title, imageUrl} = category;
+    const navigate = useNavigate()
+
+    const onNavigateHandler = () => navigate(route)
 
     return (
-        <DirectoryItemContainer>
+        <DirectoryItemContainer onClick={onNavigateHandler}>
             <BackgroundImage imageUrl={imageUrl}/>
             <Body>
                 <h2>{title}</h2>
